55366e68fc8b9ad618a260650f2844f80601ce39
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-10-30 23:21:30 +0530

    Update next.config.js

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 require("dotenv").config();
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   env: {
     API_URL: process.env.API_URL,
@@ -10,6 +12,9 @@ module.exports = {
     API_URL: process.env.API_URL,
     API_OG: process.env.API_OG,
   },
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
+}
 
   webpack: (config) => {
     config.resolve.alias["components"] = path.join(__dirname, "components");
