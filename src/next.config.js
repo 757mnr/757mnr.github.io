ec0801ac161bffca7badb7a30ec84f2ff424ec85
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-10-30 23:23:16 +0530

    Update next.config.js

--- src/next.config.js
+++ src/next.config.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
-}
+
 
   webpack: (config) => {
     config.resolve.alias["components"] = path.join(__dirname, "components");
