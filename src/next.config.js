5925b3b10547d9d3061630ccf8c2f448a3a12c3e
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-10-30 23:05:07 +0530

    Add files via upload

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,21 @@
-const isProd = process.env.NODE_ENV === 'production'
+const path = require("path");
+require("dotenv").config();
 
 module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/757mnr/757mnr.github.io/gh-pages/' : '',
-}
+  distDir: "build",
+  env: {
+    API_URL: process.env.API_URL,
+    API_OG: process.env.API_OG,
+  },
+  publicRuntimeConfig: {
+    API_URL: process.env.API_URL,
+    API_OG: process.env.API_OG,
+  },
+
+  webpack: (config) => {
+    config.resolve.alias["components"] = path.join(__dirname, "components");
+    config.resolve.alias["public"] = path.join(__dirname, "public");
+
+    return config;
+  },
+};
